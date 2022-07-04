191bcf2bd54069c8467a28d49dbf91e29414dc43
mjariwala98 <mjariwala98@gmail.com>
2022-07-04 17:29:25 +0530

    fix: folder rename

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Titles from "./components/Titles";
 import Form from "./components/Form";
 import Weather from "./components/Weather";
 
-const API_KEY = "3585775f387b0d0cba6c5b3dc41b8167";
+const API_KEY = "d3d540eb5cff04fa56a5fbf10e870978";
 
 class App extends React.Component {
   state = {
@@ -13,23 +13,36 @@ class App extends React.Component {
     country: undefined,
     humidity: undefined,
     description: undefined,
-    error: undefined
-  }
+    error: undefined,
+  };
   getWeather = async (e) => {
     e.preventDefault();
     const city = e.target.elements.city.value;
     const country = e.target.elements.country.value;
-    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
+    const api_call = await fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`
+    );
     const data = await api_call.json();
     if (city && country) {
-      this.setState({
-        temperature: data.main.temp,
-        city: data.name,
-        country: data.sys.country,
-        humidity: data.main.humidity,
-        description: data.weather[0].description,
-        error: ""
-      });
+      if (data.main) {
+        this.setState({
+          temperature: data.main ? data.main.temp : 0,
+          city: data.name,
+          country: data.sys.country,
+          humidity: data.main ? data.main.humidity : 0,
+          description: data.weather[0].description,
+          error: "",
+        });
+      } else {
+        this.setState({
+          temperature: undefined,
+          city: undefined,
+          country: undefined,
+          humidity: undefined,
+          description: undefined,
+          error: "API SERVER DOWN",
+        });
+      }
     } else {
       this.setState({
         temperature: undefined,
@@ -37,10 +50,10 @@ class App extends React.Component {
         country: undefined,
         humidity: undefined,
         description: undefined,
-        error: "Please enter the values."
+        error: "Please enter the values.",
       });
     }
-  }
+  };
   render() {
     return (
       <div>
@@ -53,8 +66,8 @@ class App extends React.Component {
                 </div>
                 <div className="col-xs-7 form-container">
                   <Form getWeather={this.getWeather} />
-                  <Weather 
-                    temperature={this.state.temperature} 
+                  <Weather
+                    temperature={this.state.temperature}
                     humidity={this.state.humidity}
                     city={this.state.city}
                     country={this.state.country}
@@ -69,6 +82,6 @@ class App extends React.Component {
       </div>
     );
   }
-};
+}
 
 export default App;
